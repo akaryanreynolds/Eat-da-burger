da8aa98e0e640f1bb06b4b59d37833cecc256211
akaryanreynolds <ryanreynolds.ca@gmail.com>
2018-01-17 16:41:12 -0800

    File Structure Created
    built files/folders

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+INSERT INTO burgers
+    (burger_name, devourer)
+VALUES
+    ("the HAMburger", 0),
+    ("PBJ burger", 0),
+    ("Bacon burger", 0)
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burger_db",
+  port:"8000"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+
+
+
+
+
+
+
+app.listen(port, function() {
+    console.log("Listening on PORT " + port);
+});
